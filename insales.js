baa02f425f7fd25fa1a201175588cd0aa08d44e0
office-one <cevportfolio@mail.ru>
2021-11-19 12:33:46 +1100

    started second task

--- insales.js
+++ insales.js
@@ -1,4 +1,5 @@
 const testTime = ["12:05am","12:01pm","10:12pm","1:20am"];
+const testPoly = ["anna","kayak"];
 
 function findMinTimeDiff(timeArray) {
     let hours = 0;
@@ -22,8 +23,25 @@ function findMinTimeDiff(timeArray) {
         } else {
             arr.push(hours * 60 + minutes);
         }
-    }    
-    console.log(arr);
+    }
+    if (arr.length > 0) {
+        minimum = Math.abs(arr[0] - arr[1]);
+        for (var i = 0; i < arr.length; i++) {
+            for (var j = i + 1; j < arr.length; j++) {
+                let curr = Math.abs(arr[i] - arr[j]);
+                console.log(curr);
+                if (curr < minimum) {
+                    minimum = curr;
+                }
+            }
+        }
+    }
+    console.log(arr + " " + minimum);
 }
 
-findMinTimeDiff(testTime);
+function findPoly(polyArray) {
+
+}
+
+// findMinTimeDiff(testTime);
+findPoly(testPoly);
